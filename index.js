4cf62d9be0659d8498b05007db41150425e69d5a
Navneet <navnet.v.maurya@gmail.com>
2022-12-07 13:43:59 +0530

    All Donegit status

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ import postRoute from "./Routes/postRoute.js";
 import dotenv from "dotenv";
 import chatRouter from "./Routes/chatRoute.js";
 import messageRouter from "./Routes/messageRouter.js";
+import { createServer } from "http";
+import { Server } from "socket.io";
 
 const app = express();
 dotenv.config();
@@ -17,10 +19,48 @@ app.use(cors());
 
 const PORT = process.env.PORT || 5000;
 
+//socket server
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+  cors: {
+    origin: "https://postalzia.netlify.app",
+  },
+});
+
+let activeUsers = [];
+
+//socket functions
+io.on("connection", (socket) => {
+  socket.on("new-user-add", (newUserId) => {
+    if (activeUsers.find((user) => user.userId === newUserId) === undefined) {
+      activeUsers.push({
+        userId: newUserId,
+        socketId: socket.id,
+      });
+    }
+
+    socket.emit("get-users", activeUsers);
+  });
+
+  socket.on("send-message", (data) => {
+    const user = data.onlineUsers.find((u) => u.userId === data.receiverId);
+
+    if (user !== undefined) {
+      socket.to(user.socketId).emit("receive-message", data.socketmessage);
+    }
+  });
+
+  socket.on("disconnect", () => {
+    activeUsers = activeUsers.filter((user) => user.socketId !== socket.id);
+    socket.emit("get-users", activeUsers);
+  });
+});
+
+//mongodb connection
 mongoose
   .connect(process.env.URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(
-    app.listen(PORT, () => {
+    httpServer.listen(PORT, () => {
       console.log(`server connected to port ${PORT}`);
     })
   )
